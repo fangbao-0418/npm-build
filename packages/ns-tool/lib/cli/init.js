4387a449f3dcb805f129102b48bc3be53869ef36
junyi <junyi.fang@tuya.com>
2022-03-22 00:34:52 +0800

    feat: init

--- packages/ns-tool/lib/cli/init.js
+++ packages/ns-tool/lib/cli/init.js
@@ -25,7 +25,7 @@ function init () {
 
   const templateDir = path.join(templatePath, 'template');
   if (fs.existsSync(templateDir)) {
-    fs.copySync(templateDir, appPath + '/src');
+    fs.copySync(templateDir, appPath);
     spawn.sync(command, ['install'], {
       stdio: 'inherit',
     });
